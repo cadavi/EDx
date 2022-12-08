7e23cc72653bf6a98a482087d22a0f8479e6c84f
cadavi <carlosdavimm@hotmail.com>
2022-12-08 09:29:16 -0400

    Delete PluralSight/BetanysShop repo
    Delete PluralSight/BetanysShop repo

--- PluralSight/BethanysPieShop/BethanysPieShop/Models/IPieRepositorycs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BethanysPieShop.Models
-{
-    public interface IPieRepository
-    {
-        IEnumerable<Pie> AllPies { get; }
-        IEnumerable<Pie> PieOfTheWeek { get; }
-        Pie? GetPiById(int pieId);
-    }
-}
